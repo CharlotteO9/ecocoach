cfed99d19c9a86e5a40b8a22b53bc9390aecd0ce
charlotte oswald <charlotteoswald9@gmail.com>
2022-11-30 11:34:42 +0100

    home ok profil with challenges ok new challenge index ok add new challenge ok but status new challenge nil instead of false

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,9 @@ Rails.application.routes.draw do
     resources :bookings, only: [:new, :create]
   end
 
-  resources :bookings, only: [:show, :edit, :update, :destroy] do
-    resources :usertips, only: [:new, :create]
-  end
+  resources :bookings, only: [:show, :edit, :update, :destroy]
 
-  resources :usertips, only: [:edit, :update, :destroy]
-  
+  resources :usertips, only: [:new, :create, :edit, :update, :destroy]
+
   get 'profile', to: 'pages#profile'
 end
