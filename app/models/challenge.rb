ff653460642279e6cf36ae551f31484b510be453
charlotte oswald <charlotteoswald9@gmail.com>
2022-12-01 14:02:10 +0100

    seed avec les user tips ok et affichage des saved tips ok

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,7 +1,7 @@
 class Challenge < ApplicationRecord
   belongs_to :seniority
   has_many :tips, dependent: :destroy
-  has_many :bookings, dependent: :destroy
+  has_many :bookings
   validates :name, :description, :category, presence: true
   validates :category, inclusion: { in: %w[Food Transport Energy Purchase Waste] }
 end
