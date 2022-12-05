4568f4ab98ae0496dcfe08f410a7211f52d8d91f
Pauline <paulinegouttiere@gmail.com>
2022-12-05 15:33:59 +0100

    🌰 cool new buttons for the search 🌰

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
-      # Uncomment when you *really understand* Pundit!
+  # Uncomment when you *really understand* Pundit!
   # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   # def user_not_authorized
   #   flash[:alert] = "You are not authorized to perform this action."
